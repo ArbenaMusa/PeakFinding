3c6401ed330506c2c7fabbd44fe74a1fa9881b58
Fatbardh <fatbardh.kadriu@gmail.com>
2021-01-13 16:53:44 +0100

    Implemented peak finder on 2d array

---
+++ PeakFinding/PeakFinder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace PeakFinding
+{
+    public static class PeakFinder
+    {
+        public static int FindPeak(int[,] matrix, int rows, int columns, int mid)
+        {
+            int[] max_function = Max(matrix, rows, mid);
+            int max_index = max_function[0];
+            int max = max_function[1];
+
+            // If we are on the first or last column, 
+            // max is a peak 
+            if (mid == 0 || mid == columns - 1)
+                return max;
+
+            // If mid column maximum is also peak 
+            if (max >= matrix[max_index, mid - 1] &&
+                max >= matrix[max_index, mid + 1])
+                return max;
+
+            // If max is less than its left 
+            if (max < matrix[max_index, mid - 1])
+                return FindPeak(matrix, rows, columns,
+                       (int)(mid - Math.Ceiling((double)mid / 2)));
+
+            // If max is less than its right 
+            // if (max < arr[max_index][mid+1]) 
+            return FindPeak(matrix, rows, columns,
+                   (int)(mid + Math.Ceiling((double)mid / 2)));
+        }
+
+        // Function to find the maximum in column 'mid' and its index, 'rows' is number of rows.
+        public static int[] Max(int[,] matrix, int rows, int mid)
+        {
+            int max_index = 0;
+            int max = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                if (max < matrix[i, mid])
+                {
+                    max = matrix[i, mid];
+                    max_index = i;
+                }
+            }
+            return new int[] { max_index, max } ;
+        }
+
+    }
+}
--- PeakFinding/Program.cs
+++ PeakFinding/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PeakFinding
 {
@@ -10,7 +6,21 @@ namespace PeakFinding
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test");
+            /*int[,] matrix = {{ 10, 8, 10, 10 },
+                                      { 14, 13, 12, 11 },
+                                      { 15, 9, 11, 21 },
+                                      { 16, 17, 19, 20 }};*/
+
+            int[,] matrix = { { 10, 20, 15 },
+                            {21, 30, 14},
+                            {7,  16, 32 } };
+
+
+            // Number of rows and columns 
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+
+            Console.WriteLine($"Peak element in the given matrix is : {PeakFinder.FindPeak(matrix, rows, columns, columns / 2)}");
         }
     }
 }
